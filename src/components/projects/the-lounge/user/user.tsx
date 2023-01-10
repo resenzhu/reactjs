7a4f7b92a741f48f9bb7ad4af543c1f79eb52475
resen-idstar <resen.zhu@gmail.com>
2023-01-10 17:19:35 +0700

    update code

--- src/components/projects/the-lounge/user/user.tsx
+++ src/components/projects/the-lounge/user/user.tsx
@@ -1,9 +1,27 @@
+import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import {faCircleDot} from '@fortawesome/free-solid-svg-icons';
 import styles from './user.module.scss';
 
-const User = (): JSX.Element =>
+type User =
+{
+  name: string,
+  status: 'online' | 'away'
+};
+
+const User = ({name, status}: User): JSX.Element =>
 (
   <div className={styles.user}>
-    USER
+    <div className={styles.name}>
+      {name}
+    </div>
+    <div className={styles.status}>
+      <div className={styles.dot}>
+        <FontAwesomeIcon icon={faCircleDot} />
+      </div>
+      <div className={styles.description}>
+        {status}
+      </div>
+    </div>
   </div>
 );
 
