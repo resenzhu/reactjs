25fc8383fe5e135630cb67e0d595023f295208a8
resen-idstar <resen.zhu@gmail.com>
2023-01-17 13:27:28 +0700

    add info menu

--- src/components/projects/the-lounge/header/header.tsx
+++ src/components/projects/the-lounge/header/header.tsx
@@ -1,16 +1,17 @@
+import {faInfoCircle, faUsers} from '@fortawesome/free-solid-svg-icons';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {MouseEventHandler} from 'react';
-import {faUsers} from '@fortawesome/free-solid-svg-icons';
 import styles from './header.module.scss';
 
 type Header =
 {
   title: string,
   subtitle?: string | (() => string),
-  onclickusers?: MouseEventHandler<HTMLDivElement>
+  onclickusers?: MouseEventHandler<HTMLDivElement>,
+  onclickinfo?: MouseEventHandler<HTMLDivElement>
 };
 
-const Header = ({title, subtitle, onclickusers}: Header): JSX.Element =>
+const Header = ({title, subtitle, onclickusers, onclickinfo}: Header): JSX.Element =>
 (
   <div className={styles.header}>
     <div className={styles.room}>
@@ -25,12 +26,18 @@ const Header = ({title, subtitle, onclickusers}: Header): JSX.Element =>
       }
     </div>
     {
-      onclickusers &&
+      (onclickusers || onclickinfo) &&
       <div className={styles.menu}>
         {
           onclickusers &&
           <div className={styles.users} onClick={onclickusers}>
             <FontAwesomeIcon icon={faUsers} />
+          </div>
+        }
+        {
+          onclickinfo &&
+          <div className={styles.info} onClick={onclickinfo}>
+            <FontAwesomeIcon icon={faInfoCircle} />
           </div>
         }
       </div>
