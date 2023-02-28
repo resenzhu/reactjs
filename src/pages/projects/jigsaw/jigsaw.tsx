c4858fcbc45572df222f4f43dbebc0c4e0ecf200
resen-idstar <resen.zhu@gmail.com>
2023-02-28 11:33:23 +0700

    add jigsaw seo

--- src/pages/projects/jigsaw/jigsaw.tsx
+++ src/pages/projects/jigsaw/jigsaw.tsx
@@ -1,5 +1,17 @@
 import 'react-jigsaw-puzzle/lib/jigsaw-puzzle.css';
+import {Seo} from './../../../components/main';
+import {useTranslation} from './../../../hooks/main';
 
-const Jigsaw = (): JSX.Element => <></>;
+const Jigsaw = (): JSX.Element =>
+{
+  const {translate} = useTranslation('project.jigsaw');
+
+  return (
+    <Seo
+      title={translate('seo.title')}
+      description={translate('seo.description')}
+    />
+  );
+};
 
 export default Jigsaw;
