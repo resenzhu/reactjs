f21b0a36f66ba9c88d9a920a9f881a3bf8a5d897
Resen <resen.zhu@gmail.com>
2023-05-07 23:49:35 +0700

    remove layout and seo

--- src/components/main/index.tsx
+++ src/components/main/index.tsx
@@ -1,7 +1,5 @@
 export {default as Button} from './button/button';
 export {default as Form} from './form/form';
 export {default as Headline} from './headline/headline';
-export {default as Layout} from './layout/layout';
 export {default as Project} from './project/project';
-export {default as Seo} from './seo/seo';
 export {default as Social} from './social/social';
